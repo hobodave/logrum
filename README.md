0165526b6e0e1f3c438fb3b883fd2708ce77e794
David Abdemoulaie <dave@hobodave.com>
2010-12-22 13:08:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# LogRum #
+
+LogRum is a simple web-based log tailer inspired by [clarity][1] and a 
+desire to satisfy my curiosity in node.js.
+
+# Attributions & License #
+
+LogRum is released under a BSD License. See LICENSE.
+
+<hr/>
+
+Portions of the HTML & CSS are copied verbatim from [clarity][1] and are
+subject to the following MIT license:
+
+<pre>
+Copyright © 2009 Tobias Lütke
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ‘Software’), to 
+deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+</pre>
+
+
+
+
+  [1]: https://github.com/tobi/clarity
---
+++ tail.js
@@ -0,0 +1,129 @@
+var sys = require('sys')
+  , fs = require('fs')
+  , url = require('url')
+  , http = require('http')
+  , io = require('socket.io')
+  , yaml = require('yaml')
+  , glob = require('glob')
+  , httpdigest = require('http-digest')
+  , Mustache = require('mustache')
+  , spawn = require('child_process').spawn;
+
+var parseConfig = function(filename) {
+  return yaml.eval(fs.readFileSync(filename, 'utf8'));
+}
+
+var config = parseConfig('./config/config.yml')
+
+var logs = []
+for (var i=0; i < config.logs.length; i++) {
+  files = glob.globSync(config.logs[i], 0);
+  for (var j=0; j < files.length; j++) {
+    logs.push(files[j]);
+  };
+};
+
+var log_opts = [];
+for (var i=0; i < logs.length; i++) {
+  log_opts.push({idx: i, name: logs[i]});
+};
+
+var actions = [];
+actions.push({
+  path: "/",
+  template: "tail.html",
+  view: {
+    log_opts: log_opts
+  }
+})
+var processes = {}
+
+var server = httpdigest.createServer("admin", "pass", function(req, res) {
+
+  req.addListener('end', function() {
+    var action = actions.filter(function(a) { return req.url === a.path });
+    action = (action.length > 0 ? action[0] : undefined);
+    
+    if ('undefined' == typeof action) {
+      var path = url.parse(req.url).pathname;
+      switch (path){
+        case '/json.js':
+          fs.readFile(__dirname + path, function(err, data){
+            if (err) return send404(res);
+            res.writeHead(200, {'Content-Type': 'text/javascript'})
+            res.end(data, 'utf8');
+          });
+          break;
+        default: send404(res);
+      }
+    } else {
+      fs.readFile('./views/' + action.template, 'utf8', function(err, template) {
+        if (err) {
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.end(''+err);
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          res.write(Mustache.to_html(template, action.view))
+          res.end()
+        }
+      })
+    }
+  });
+}),
+
+send404 = function(res){
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.end('404 - Page Not Found');
+};
+
+server.listen(config.port);
+
+var io = io.listen(server);
+
+io.on('connection', function(client){
+
+  var sessionId = client.sessionId;
+  var stringdata = '';
+  var restdata = '';
+  
+  processes[sessionId] = {
+    active_log: null,
+    tail: null
+  }
+
+  var tail_start = function(active) {
+    processes[sessionId].active_log = active
+    processes[sessionId].tail = spawn("tail", ["-f", "-n", 250, logs[active]])
+    processes[sessionId].tail.stdout.setEncoding('utf8');
+    processes[sessionId].tail.stdout.on("data", function (data) {
+      stringdata = restdata + data;
+      linedata = stringdata.substr(0, stringdata.lastIndexOf('\n')).split('\n');
+      restdata = stringdata.substr(stringdata.lastIndexOf('\n') + 1);
+
+      buffer = []
+      for (var i=0; i < linedata.length; i++) {
+        buffer.push({ message: linedata[i] });
+      };
+      client.send({ buffer: buffer });
+      buffer = []
+    })
+  }
+
+  var tail_stop = function() {
+    if (processes[sessionId].tail !== null) processes[sessionId].tail.kill();
+  }
+
+  client.on('message', function(message) {
+    console.log(sessionId + ': ' + message)
+    active = parseInt(message)
+    if (active !== processes[sessionId].active_log) {
+      tail_stop();
+      client.send({flush: true})
+      tail_start(active);
+    }
+  });
+
+  client.on('disconnect', function(){
+    tail_stop();
+  });
+});
